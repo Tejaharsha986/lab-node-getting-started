4e18d30d2430fcc69c54018b327878033df54bd7
Teja <makkenabrothers143@gmail.com>
2022-02-01 20:34:43 +0530

    First Message

--- index.js
+++ index.js
@@ -1,28 +1,72 @@
 class SortedList {
-  constructor() {
+  constructor(items, length) {
+    this.items = []
+    this.length = 0
+  }
+  add(item) {
+   this.length =this.length+1;
+   const items=this.items;
+   items.push(item)
+   items.sort((a,b)=> a-b)
+   this.items =items
+  }
+  get(pos) {
   
-  }
-  add() {
-   
-  }
-  get() {
-
+    if(pos >= this.length)
+    {
+      throw new Error('OutOfBounds');
+      return;
+    }
+    return this.items[pos];
   }
   max() {
-
+    if(this.items.length === 0)
+    {
+      throw new Error("EmptySortedList");
+      return;
+    }
+    let num = Number.MIN_VALUE;
+    this.items.map((item) =>{
+      if(item > num){
+        num = item;
+      }
+    });
+    return num;
   }
   min() {
-
+    
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+      return;
+    }
+    let num = Number.MAX_VALUE;
+    this.items.map((item)=>{
+      if(item < num){
+        num = item;
+      }
+    });
+    return num;
   }
   avg() {
-    
-   
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+      return;
+    }
+    let num = 0;
+    this.items.map(item =>{
+      num+=item
+    })
+    return(num/this.length);
   }
 
   sum() {
-  
+    let num = 0;
+    this.items.map(item =>{
+      num+=item
+    })
+    return num;
   }
   
-};
+}
 
 module.exports = SortedList;
